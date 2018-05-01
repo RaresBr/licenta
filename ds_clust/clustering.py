1c7e26ac378d48a0002fd2a944ec2f87fc807551
RaresBr <rares.bradea@gmail.com>
2018-05-01 18:21:31 +0300

    work on lda. now can show topic of document. maybe not enough clusters?

--- ds_clust/clustering.py
+++ ds_clust/clustering.py
@@ -1,9 +1,8 @@
 from __future__ import print_function
 from pyspark import SparkConf, SparkContext
 from pyspark.ml import Pipeline
-from pyspark.ml.clustering import KMeans, LDA
+from pyspark.ml.clustering import KMeans, LDA, DistributedLDAModel
 from pyspark.ml.feature import Tokenizer, StopWordsRemover, HashingTF, IDF
-
 
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.decomposition import TruncatedSVD
@@ -16,7 +15,7 @@ from sklearn import metrics
 
 from pyspark.sql import SparkSession
 from pyspark.sql import SQLContext
-
+import numpy as np
 conf = SparkConf()
 conf.setAppName("TwitterStreamApp")
 
@@ -27,7 +26,8 @@ sqlContext = SQLContext(sc)
 
 
 def kmeansresults():
-    df1 = sqlContext.read.format("csv").option("header", "true").option("mode", "DROPMALFORMED").load("canadatweets.csv")
+    df1 = sqlContext.read.format("csv").option("header", "true").option("mode", "DROPMALFORMED").load \
+        ("canadatweets.csv")
     df2 = sqlContext.read.format("csv").option("header", "true").option("mode", "DROPMALFORMED").load(
         "products.csv")
     df3 = sqlContext.read.format("csv").option("header", "true").option("mode", "DROPMALFORMED").load(
@@ -42,66 +42,64 @@ def kmeansresults():
 
     tokenizer = Tokenizer(inputCol="text", outputCol="tokens")
     remover = StopWordsRemover(inputCol="tokens", outputCol="stopWordsRemovedTokens")
-    hashingTF = HashingTF(inputCol="stopWordsRemovedTokens", outputCol="rawFeatures", numFeatures=2**20)
+    hashingTF = HashingTF(inputCol="stopWordsRemovedTokens", outputCol="rawFeatures", numFeatures= 2 **20)
     idf = IDF(inputCol="rawFeatures", outputCol="features")
 
-    kmeans = KMeans(k=8, seed=1,featuresCol='rawFeatures',maxIter=10,initMode='random')
+    kmeans = KMeans(k=8, seed=1 ,featuresCol='rawFeatures' ,maxIter=10 ,initMode='random')
     pipeline = Pipeline(stages=[tokenizer, remover, hashingTF,  kmeans])
     model = pipeline.fit(df)
     results = model.transform(df)
     results.cache()
     results.groupBy("prediction").count().show()  # Note "display" is for Databricks; use show() for OSS Apache Spark
-    #results.filter(results.prediction == 1).show(200,False)
+    # results.filter(results.prediction == 1).show(200,False)
     results.show()
     results.toPandas().to_csv('kmeansresultsCanadaAndProductsAndDisastersAndClaritin.csv')
 
 
 def ldaresults():
-    df1 = sqlContext.read.format("csv").option("header", "true").option("mode", "DROPMALFORMED").load("canadatweets.csv")
+    df1 = sqlContext.read.format("csv").option("header", "true").option("mode", "DROPMALFORMED").load \
+        ("canadatweets.csv")
     df2 = sqlContext.read.format("csv").option("header", "true").option("mode", "DROPMALFORMED").load(
-        "disastersSmall400.csv")
+        "products.csv")
+    df3 = sqlContext.read.format("csv").option("header", "true").option("mode", "DROPMALFORMED").load(
+       "products.csv")
+    df4 = sqlContext.read.format("csv").option("header", "true").option("mode", "DROPMALFORMED").load(
+        "claritin.csv")
     df = df1.unionAll(df2)
-    # df.show()
+    df = df.unionAll(df3)
+    df = df.unionAll(df4)
+    df.show()
     # df2.show()
 
     tokenizer = Tokenizer(inputCol="text", outputCol="tokens")
     remover = StopWordsRemover(inputCol="tokens", outputCol="stopWordsRemovedTokens")
-    hashingTF = HashingTF(inputCol="stopWordsRemovedTokens", outputCol="rawFeatures", numFeatures=2**18)
+    hashingTF = HashingTF(inputCol="stopWordsRemovedTokens", outputCol="rawFeatures", numFeatures= 2 **18)
     idf = IDF(inputCol="rawFeatures", outputCol="features", minDocFreq=5)
 
     kmeans = KMeans(k=20, seed=1)
-    lda = LDA(k=2, seed=1, optimizer="em", featuresCol='features')
+    lda = LDA(k=4, seed=1, optimizer="em", featuresCol='features')
 
-    pipeline = Pipeline(stages=[tokenizer, remover, hashingTF, idf,lda])
+    pipeline = Pipeline(stages=[tokenizer, remover, hashingTF, idf, lda])
     model = pipeline.fit(df)
     topics = model.stages[-1].describeTopics()
 
-    topics.show(truncate=False)
+    # topics.show(truncate=False)
     transformed = model.transform(df)
-    transformed.sort('topicDistribution').show(20000,truncate=False)
-    transformed.toPandas().to_csv('ldaresultsdisAndCana.csv')
 
-kmeansresults()
-#ldaresults()
+    # transformed.sort('topicDistribution').show(20000,truncate=False)
+    # transformed.toPandas().to_csv('ldaresultsCanadaAndProductsAndDisastersAndClaritin.csv')
+    transformed.rdd.map(lambda row: (row['text'] ,row['features'] ,row['topicDistribution'],int(np.argmax(np.asarray([float(x) for x in row['topicDistribution']])))))\
+        .toDF()\
+        .toPandas().to_csv('ldaresultsCanadaAndProductsAndDisastersAndClaritin.csv')
 
-def scikit():
-    categories = [
-        'alt.atheism',
-        'talk.religion.misc',
-        'comp.graphics',
-        'sci.space',
-    ]
-    print("Loading 20 newsgroups dataset for categories:")
-    print(categories)
 
-    dataset = fetch_20newsgroups(subset='all', categories=categories,
-                                 shuffle=True, random_state=42)
+    # transformed.withColumn('TopicNumber',lit(np.argmax(np.asarray(list(transformed.topicDistribution))))).show(5,truncate=False)
+    # model.stages[-1].save("LDAModel")
 
-    print("%d documents" % len(dataset.data))
-    print("%d categories" % len(dataset.target_names))
-    print(dataset.data)
-    file = open('twenty.csv','w')
-    for line in dataset.data:
-        file.write(line)
 
-#scikit()
+# kmeansresults()
+ldaresults()
+
+
+
+
