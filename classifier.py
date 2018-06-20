c73811da45222027f4455210f26183a5b6420465
RaresBr <rares.bradea@gmail.com>
2018-06-20 17:23:25 +0300

    finish classification chapter

--- classifier.py
+++ classifier.py
@@ -18,6 +18,25 @@ def score(model):
     accuracy = elements_gotten_right / float(all_elements)
     return accuracy
 
+def score_new(model):
+    features = []
+    for element in test_data:
+        features.append(element.features)
+
+    predictions = model.predict(features)
+
+    labels = []
+    for element in test_data:
+        labels.append(element.label)
+
+    labels_with_predictions = zip(labels, predictions)
+
+    elements_gotten_right = []
+    for element in labels_with_predictions:
+        if element[0] == element[1]:
+            elements_gotten_right.append(element)
+    return len(elements_gotten_right) / float(len(test_data))
+
 
 conf = SparkConf()
 conf.setAppName("TweetClassifier")
@@ -30,52 +49,54 @@ real = sc.textFile("a0newCSVFullTweets_cleanLOWERCASE.txt")
 fake_words = fake.map(lambda sentence: sentence.split())
 real_words = real.map(lambda sentence: sentence.split())
 
-print(fake_words.take(3))
-print(real_words.take(3))
+print(fake_words.take(1))
+print(real_words.take(1))
 
 tf = HashingTF(numFeatures=2 ** 18)
 fake_features = tf.transform(fake_words)
 real_features = tf.transform(real_words)
 
-print(fake_features.take(3))
-print(real_features.take(3))
+print(fake_features.take(1))
+print(real_features.take(1))
 
 # label each element; either fake or real sentence
 fake_samples = fake_features.map(lambda features: LabeledPoint(1, features))
 real_samples = real_features.map(lambda features: LabeledPoint(0, features))
 
-print(fake_samples.take(3))
-print(real_samples.take(3))
+print(fake_samples.take(1))
+print(real_samples.take(1))
 
 samples = fake_samples.union(real_samples)
 [training_data, test_data] = samples.randomSplit([0.8, 0.2])
 training_data.cache()
 test_data.cache()
 
-# algorithm = LogisticRegressionWithSGD()
-# model = algorithm.train(training_data)
-# print('logistic regression sgd:' ,score(model))
-#
-# algorithm = LogisticRegressionWithLBFGS()
-# model = algorithm.train(training_data)
-# print('logistic regression with lbfgs: ',score(model))
-#
-#
+algorithm = LogisticRegressionWithSGD()
+model = algorithm.train(training_data)
+print('logistic regression sgd:', score(model))
+
+algorithm = LogisticRegressionWithLBFGS()
+model = algorithm.train(training_data)
+print('logistic regression with lbfgs:', score(model))
+
 # algorithm = DecisionTree()
 # model = algorithm.trainClassifier(training_data, numClasses=2,categoricalFeaturesInfo={})
 # print('decision tree: ',score(model))
-
+#
 # algorithm = RandomForest()
 # model = algorithm.trainClassifier(training_data,numClasses=2,categoricalFeaturesInfo={},numTrees=16)
-# print('ranom forest: ',score(model))
+# print('random forest: ',score(model))
 
 algorithm = NaiveBayes()
 model = algorithm.train(training_data)
-print('naive bayes: ',score(model))
+print('naive bayes: ', score(model))
 
 algorithm = SVMWithSGD()
-model = algorithm.train(training_data,iterations=10)
-print('svm with sgd: ',score(model))
-#model.save(sc,"classifierModelPLOS")
+model = algorithm.train(training_data, iterations=10)
+print('svm with sgd: ', score(model))
+# model.save(sc,"classifierModelPLOS")
 if __name__ == "__main__":
     pass
+
+
+
